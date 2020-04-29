6176a292abf9a17cafef14252784312605927acf
Novax69 <damien.petitjean@laposte.net>
2020-04-29 10:40:53 +0200

    Global Variable

--- mpmission/config/Config_Master.hpp
+++ mpmission/config/Config_Master.hpp
@@ -1,9 +1,11 @@
 class Life_Settings {
 	/* Add at the beginning */
+	//Global variable for all my script, if you already have it don't add it to you config_master
+	nov_useDonator = false; //Enable donators bonuses
+
 
 	//Params used for the Livret A
 	nov_depositLimit = 10000; // set up the max ammount that can be deposit on the livret A
-	nov_useDonator = false; // Bigger % for donators and Bigger deposit limit
 	nov_numberAddedDonor = 10000; // This number add 10.000 * donor level as limit
 	nov_percentToAdd = 2; // 2 % added every 30 minutes, can be changed; for donors it's LEVEL divided by 4 added to the percent here
 	// Ends Livret A Params 
